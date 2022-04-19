594516206394c690fd394def7b7e5fe49b11e513
DarkedgeTH <darkedgeth@gmail.com>
2022-04-19 16:11:28 -0400

    POST request for /api/cars working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,7 +1,12 @@
 // DO YOUR MAGIC
 const express = require('express')
 const Car = require('./cars-model')
-const { checkCarId } = require('./cars-middleware')
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique
+} = require('./cars-middleware')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
@@ -17,8 +22,18 @@ router.get('/:id', checkCarId, async (req, res, next) => {
     res.json(req.car)
 })
 
-router.post('/', async (req, res, next) => {
-    res.json('posting new car')
-})
+router.post(
+    '/',
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+    async (req, res, next) => {
+        try {
+            const car = await Car.create(req.body)
+            res.json(car)
+        } catch (err) {
+            next(err)
+        }
+    })
 
 module.exports = router
